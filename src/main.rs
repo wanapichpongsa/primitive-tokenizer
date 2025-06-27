8e39658df99f72fcfacb3acef4a1e07d80454d91
Wana Pichpongsa <wana@pichpongsa.com>
2025-06-27 14:59:38 +0100

    feat: tokens Hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 /*
     Making a tokenizer...
@@ -14,7 +14,20 @@ fn unique_chars_sorted_ascii(text: &str) -> String {
     chars.into_iter().collect()
 }
 
+fn tokenize(text: &str) -> HashMap<i32, char> {
+    let mut tokens: HashMap<i32, char> = HashMap::new();
+    let mut index: i32 = 0;
+    for c in text.chars() {
+        tokens.insert(index, c);
+        index += 1;
+    }
+    return tokens;
+}
+
 fn main() {
-    let text = "Rust is fun to learn".to_string();
-    println!("{}", unique_chars_sorted_ascii(&text));
+    let text: String = "Rust is fun to learn".to_string();
+    let chars: String = unique_chars_sorted_ascii(&text);
+    // Follows Hash bucket order not insertion order.
+    let tokens: HashMap<i32, char> = tokenize(&chars);
+    println!("{:?}", tokens);
 }
