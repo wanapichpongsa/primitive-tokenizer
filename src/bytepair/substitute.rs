c096be4216bb5fdf53759a667126a3bd7302ed5a
Wana Pichpongsa <wana@pichpongsa.com>
2025-06-28 09:52:25 +0100

    test: bytepair - success

--- src/bytepair/substitute.rs
+++ src/bytepair/substitute.rs
@@ -3,6 +3,7 @@ use std::vec::Vec;
 pub fn substitute_pairs(tokens: &Vec<u8>, most_frequent: Vec<[u8; 2]>) -> Vec<u16> {
   let mint: u16 = 256;
   let most_freq_pair: [u8; 2] = most_frequent[0];
+  println!("{:?}", most_freq_pair);
 
   let mut new_tokens: Vec<u16> = Vec::new();
   let mut matched: bool = false;
